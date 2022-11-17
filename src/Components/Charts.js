40df92e96bd9e4b7c373c3515d900b841a7d2afd
Pravin Bhadane <pravinbhadane60@gmail.com>
2022-11-17 16:01:02 +0530

    completed

---
+++ src/Components/Charts.js
@@ -0,0 +1,38 @@
+import Chart from "react-google-charts";
+
+
+const data = [
+
+  ['Month','OnePlus','Samsung'],
+  ['Jan', 40, 35],
+  ['Feb', 48, 40],
+  ['Mar', 55, 60],
+  ['Apri', 70, 68],
+  ['May', 80, 75],
+];
+const Charts =() => {
+
+    return (
+      <div className="container mt-5">
+        <h2>Bar Chart and Line Chart</h2>
+        <Chart
+          width='90%'
+          height='450px'
+          chartType="ComboChart" 
+          data={data}
+          options={
+            { 
+            title: 'Monthly Sale  of Mobile Shop',
+            vAxis: { title: 'Quantity' },
+            hAxis: { title: 'Month' },
+            seriesType: 'bars',
+            series: {1: { type: 'line'} },
+           
+          
+          }}
+       
+        />
+      </div>
+    )
+  }
+export default Charts;
